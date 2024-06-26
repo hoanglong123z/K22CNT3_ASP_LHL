06a37a14357dc19012085f2adf1b0b1be6f2d8cb
lê Hoàng Long <lehoanglong26072004@gmail.com>
2024-06-26 10:40:39 +0700

    ls09

---
+++ LHL_lesson09/LHL_lesson09/Controllers/lhlSinhViensController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LHL_lesson09.Models;
+
+namespace LHL_lesson09.Controllers
+{
+    public class lhlSinhViensController : Controller
+    {
+        private Lesson09Entities1 db = new Lesson09Entities1();
+
+        // GET: lhlSinhViens
+        public ActionResult LHLIndex()
+        {
+            var sinhViens = db.SinhViens.Include(s => s.Khoa);
+            return View(sinhViens.ToList());
+        }
+
+        // GET: lhlSinhViens/Details/5
+        public ActionResult LHLDetails(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SinhVien sinhVien = db.SinhViens.Find(id);
+            if (sinhVien == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sinhVien);
+        }
+
+        // GET: lhlSinhViens/Create
+        public ActionResult LHLCreate()
+        {
+            ViewBag.MaKH = new SelectList(db.Khoas, "MaKH", "TenKH");
+            return View();
+        }
+
+        // POST: lhlSinhViens/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LHLCreate([Bind(Include = "MaSV,HoSV,TenSV,Phai,NgaySinh,NoiSinh,MaKH,HocBong,DiemTrungBinh")] SinhVien sinhVien)
+        {
+            if (ModelState.IsValid)
+            {
+                db.SinhViens.Add(sinhVien);
+                db.SaveChanges();
+                return RedirectToAction("LHLIndex");
+            }
+
+            ViewBag.MaKH = new SelectList(db.Khoas, "MaKH", "TenKH", sinhVien.MaKH);
+            return View(sinhVien);
+        }
+
+        // GET: lhlSinhViens/Edit/5
+        public ActionResult LHLEdit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SinhVien sinhVien = db.SinhViens.Find(id);
+            if (sinhVien == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MaKH = new SelectList(db.Khoas, "MaKH", "TenKH", sinhVien.MaKH);
+            return View(sinhVien);
+        }
+
+        // POST: lhlSinhViens/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LHLEdit([Bind(Include = "MaSV,HoSV,TenSV,Phai,NgaySinh,NoiSinh,MaKH,HocBong,DiemTrungBinh")] SinhVien sinhVien)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sinhVien).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("LHLIndex");
+            }
+            ViewBag.MaKH = new SelectList(db.Khoas, "MaKH", "TenKH", sinhVien.MaKH);
+            return View(sinhVien);
+        }
+
+        // GET: lhlSinhViens/Delete/5
+        public ActionResult LHLDelete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SinhVien sinhVien = db.SinhViens.Find(id);
+            if (sinhVien == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sinhVien);
+        }
+
+        // POST: lhlSinhViens/Delete/5
+        [HttpPost, ActionName("LHLDelete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult LHLDeleteConfirmed(string id)
+        {
+            SinhVien sinhVien = db.SinhViens.Find(id);
+            db.SinhViens.Remove(sinhVien);
+            db.SaveChanges();
+            return RedirectToAction("LHLIndex");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Lesson09.sql
@@ -0,0 +1,33 @@
+CREATE TABLE Khoa (
+    MaKH Nchar(10) PRIMARY KEY, 
+    TenKH NVARCHAR(255)
+);
+
+
+CREATE TABLE SinhVien (
+    MaSV NCHAR(10) PRIMARY KEY,  
+    HoSV NVARCHAR(50),
+    TenSV NVARCHAR(50),
+    Phai Bit,
+    NgaySinh DATE,
+    NoiSinh NVARCHAR(255),
+    MaKH NCHAR(10), 
+    HocBong nvarchar(20),
+    DiemTrungBinh DECIMAL(3, 2),
+    FOREIGN KEY (MaKH) REFERENCES Khoa(MaKH) 
+);
+
+
+CREATE TABLE MonHoc (
+    MaMH NCHAR(10) PRIMARY KEY,  
+    TenMH NVARCHAR(255),
+    Sotiet INT
+);
+CREATE TABLE Ketqua (
+    MaSV NCHAR(10),  
+    MaMH NCHAR(10),  
+    Diem DECIMAL(3, 2),
+    PRIMARY KEY (MaSV, MaMH), 
+    FOREIGN KEY (MaSV) REFERENCES SinhVien(MaSV),  
+    FOREIGN KEY (MaMH) REFERENCES MonHoc(MaMH)  
+);
